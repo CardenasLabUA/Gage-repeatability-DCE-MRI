2ae814b63eda1b8cfb16b2b1f399db2822d1de30
Julio Cardenas <cardenaj@email.arizona.edu>
2016-07-15 16:24:57 -0700

    Functions added Initial Commit

---
+++ Cp10.m
@@ -0,0 +1,15 @@
+function [Cp_out] = Cp10(t)
+%Input:  time t in minutes
+%THIS FUNCTION CALCULATES AN AIF WITH A SIMULATED INJECTION TIME OF 10
+%SECONDS
+%Injection of 10 seconds
+A= 30 ; %mM/min
+B= 1.0  ;
+C= 4  ; %min^-1
+D= .65 ; %mM 
+E= 5  ; %min
+F= .04 ;  %min-1 
+
+Cp_out=A.*(t.^B).*exp(-t.*C)+ (D.*(1-exp(-t.*E)).* exp(-t.*F));%inject = 10sec
+
+end
---
+++ myToftsCt2.m
@@ -0,0 +1,31 @@
+function [c_toi] = myToftsCt2(ktrans,kep,t,Cp)
+%
+% [c_toi] =myToftsCt2(ktrans,kep,t,Cp)
+% ktrans=pars(1);
+% kep=pars(2);
+% t=X(:,1);
+% Cp=X(:,2);
+
+
+n_points=length(t);
+% expo=zeros(1,n_points);
+% crpexp=expo;
+% c_toi=crpexp;
+
+
+
+for k = 2:n_points
+    int_t = t(k);
+    
+    for j = 1:k
+        dummy_t = t(j);
+        expo(j) =exp(-((kep.*(int_t-dummy_t))));
+        crpexp(j) = Cp(j)*expo(j);
+    end
+    t2 = t(1:k);
+    %%
+    crpexp_integral = trapz(t2,crpexp);
+    %     crpexp_integral = trapzfm(t2,crpexp); % quicker?
+    c_toi(k) = ktrans*crpexp_integral;
+end
+
